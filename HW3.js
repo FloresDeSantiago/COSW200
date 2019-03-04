fe738d61879652b4e0c3fa323705f6f2a4c11f40
OldWorldForest <oldworldforest@gmail.com>
2019-03-03 22:49:50 -0800

    basic

--- HW3.js
+++ HW3.js
@@ -1,4 +1,9 @@
+
 str = prompt("Enter a string of 25 characters including spaces");
 
+if(str.length < 25) {
+    alert("The string has to be greater than 25 characters");
+}
+
 alert(str);
-alert(str);
+
