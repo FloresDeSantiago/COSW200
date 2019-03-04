d0ac396a36f2acd0853d2c6ab69209c8afff9c1f
OldWorldForest <oldworldforest@gmail.com>
2019-03-04 10:12:12 -0800

    completed string method

--- HW3/HW3.js
+++ HW3/HW3.js
@@ -1,13 +1,19 @@
 
-str = prompt("Enter a string of 25 characters including spaces");
-
-if(str.length < 25) {
-    alert("The string has to be greater than 25 characters");
-}
 
 
-stringMethod(str);
 
+do {
+    str = prompt("Enter a sentence that has more than 25 characters: ");
+
+    if (str.length < 25) {
+        alert("The string has to be greater than 25 characters. Please enter another string.");
+    } else {
+
+        stringMethod(str);
+
+    }
+
+}while(str.length <= 25)
 
 function stringMethod(str) {
 
@@ -16,7 +22,12 @@ function stringMethod(str) {
     document.write("Original String: " + str + "<br>");
     document.write("String in lowercase: " + str.toLowerCase() + "<br>");
     document.write("String in uppercase: " + str.toUpperCase() + "<br>");
-    document.write("Your string is " + str.length + " characters long." + "<br>")
+    document.write("Your string is " + str.length + " characters long." + "<br>");
+    let newStr = str.slice(5,11);
+    document.write(newStr);
+    let newStr2 = str.substring(8,5);
+    document.write(newStr2);
+
 
     document.write(myQuote + " " + myQuote)
 
@@ -49,18 +60,18 @@ Create a program in CodePen that prompts for the input of at least five words (a
 
 STRING METHODS
 
-TODO Check to see if the string input meets the 25+ character limit. If it does not, send a message and ask for another string.
+TODO Check to see if the string input meets the 25+ character limit. If it does not, send a message and ask for another string. ----------- DONE
 TODO Output the original string as it was entered onto the web page document. ---------------------------------------------------------- DONE
 TODO Output the original string in all lower case letters. Do not change the original string value. ------------------------------------------ DONE
 TODO Output the original string in all upper case letters. Do not change the original string value. ----------------------------------------- DONE
 TODO Output a message indicating the total characters within the string. ----------------------------------------------------------------- DONE
-TODO Output the characters between index 5 and index 10.
-TODO Output 5 characters starting from position 8.
+TODO Output the characters between index 5 and index 10. ----------------------------------------------------------------- DONE
+TODO Output 5 characters starting from position 8. ----------------------------------------------------------------- DONE
 TODO Combine the string with another string of your choice that also has 25+ characters. Output the new string. ----------------------- DONE
 TODO Finally, split your string into an array for the 2nd part of the assignment. Each word (not each character) should be a value for the array.
 
 
-ARRAY METHODS
+todo ---------------------------------------------ARRAY METHODS---------------------------------------------
 TODO ----------------------------------------------------------------------------------------------------------------------------------
 TODO Output the list of array values.
 TODO Output the total number of items in the array.
