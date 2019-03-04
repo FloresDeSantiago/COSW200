7d3f55e4f5f860d66339fbeb7c7f1777accd675b
OldWorldForest <oldworldforest@gmail.com>
2019-03-03 23:13:05 -0800

    four off the todo list

--- HW3/HW3.js
+++ HW3/HW3.js
@@ -5,5 +5,66 @@ if(str.length < 25) {
     alert("The string has to be greater than 25 characters");
 }
 
-alert(str);
 
+stringMethod(str);
+
+
+function stringMethod(str) {
+    document.write("Original String: " + str + "<br>");
+    document.write("String in lowercase: " + str.toLowerCase() + "<br>");
+    document.write("String in uppercase: " + str.toUpperCase() + "<br>");
+    document.write("Your string is " + str.length + " characters long." + "<br>")
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+Create a program in CodePen that prompts for the input of at least five words (a total of 25+ characters) string from a user and performs the following functions to it:
+
+STRING METHODS
+
+TODO Check to see if the string input meets the 25+ character limit. If it does not, send a message and ask for another string.
+TODO Output the original string as it was entered onto the web page document. ----------------------------------------------------------DONE
+TODO Output the original string in all lower case letters. Do not change the original string value. ------------------------------------------DONE
+TODO Output the original string in all upper case letters. Do not change the original string value. ------------------------------------------DONE
+TODO Output a message indicating the total characters within the string. ------------------------------------------------------------------DONE
+TODO Output the characters between index 5 and index 10.
+TODO Output 5 characters starting from position 8.
+TODO Combine the string with another string of your choice that also has 25+ characters. Output the new string.
+TODO Finally, split your string into an array for the 2nd part of the assignment. Each word (not each character) should be a value for the array.
+
+
+ARRAY METHODS
+
+TODO Output the list of array values.
+TODO Output the total number of items in the array.
+TODO Remove the first item in the array. Output the new array values.
+TODO Add the word "banana" to the end of the array. Output the new array values.
+TODO Reverse the elements in the array. Output the new array values.
+TODO Replace the item in the 2nd position/key with the word stardust. Output the new array values showing the word in the correct position.
+TODO Sort the array in alphabetical order. Output the alphabetical list of array values.
+
+
+
+ */
