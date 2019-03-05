2a5812fc3264366b8f4be3c13d0af3d57ddf857a
OldWorldForest <oldworldforest@gmail.com>
2019-03-05 11:26:47 -0800

    Added some CSS, fixed minor issues and labed it

--- HW3/HW3.js
+++ HW3/HW3.js
@@ -1,4 +1,4 @@
-
+// String Method - 1
 do {
     str = prompt("Enter a sentence that has more than 25 characters: ");
 
@@ -11,39 +11,56 @@ do {
 
     }
 
-}while(str.length <= 25)
+}while(str.length <= 25);
 
 function stringMethod(str) {
     let myQuote = "There is nothing new under the sun. It has all been done before.";
-
     document.write("<h1>String Method</h1>");
-    document.write("Original String: " + str + "<br>");
-    document.write("String in lowercase: " + str.toLowerCase() + "<br>");
-    document.write("String in uppercase: " + str.toUpperCase() + "<br>");
-    document.write("Your string is " + str.length + " characters long." + "<br>");
-    let newStr = str.slice(5,11);
-    document.write(newStr  + "<br>");
+    // String Method - 2
+    document.write("<strong>Original String:</strong> " + str + "<br>");
+    // String Method - 3
+    document.write("<strong>String in lowercase:</strong> " + str.toLowerCase() + "<br>");
+    // String Method - 4
+    document.write("<strong>String in uppercase:</strong> " + str.toUpperCase() + "<br>");
+    // String Method - 5
+    document.write("<strong>Your string is</strong> " + str.length + "<strong> characters long.</strong>" + "<br>");
+    let newStr = str.slice(5,10);
+    // String Method - 6
+    document.write("<strong>Index 5 and index 10: </strong>" + newStr  + "<br>");
     let newStr2 = str.substr(8,5);
-    document.write(newStr2  + "<br>");
-    document.write(myQuote + " " + str  + "<br>")
+    // String Method - 7
+    document.write("<strong>Position 8 with the next 5 characters: </strong>" + newStr2  + "<br>");
+    // String Method - 8
+    document.write("<strong>Combine with another string: </strong>" + myQuote + " " + str  + "<br>")
 
 }
 
 
 function arrayMethod(str) {
+    // String Method - 9
     let newStr3 = str.split(" ");
     document.write("<h1>Array Method</h1>");
-    document.write("List of array values: " + newStr3  + "<br>");
-    document.write("Total number of items in the array are: " + newStr3.length + "<br>");
+    // Array method - 1
+    document.write("<strong>List of array values:</strong> " + newStr3  + "<br>");
+    // Array method - 2
+    document.write("<strong>Total number of items in the array are:</strong> " + newStr3.length + "<br>");
+    // Array method - 3
     newStr3.shift();
-    document.write("Remove first item from array: " + newStr3 + "<br>")
+    document.write("<strong>Remove first item from array:</strong> " + newStr3 + "<br>");
+    // Array method - 4
     newStr3.push("banana");
-    document.write("Pushed banana into the mix: " + newStr3 + "<br>");
+    document.write("<strong>Pushed banana into the mix:</strong> " + newStr3 + "<br>");
+    // Array method - 5
     let newStr4 = newStr3.reverse();
-    document.write("Reverse array: " + newStr4 + "<br>");
-    document.write(newStr3[1] + "<br>");
+    document.write("<strong>Reverse array:</strong> " + newStr4 + "<br>");
+
+    //document.write(newStr3[1] + "<br>");
+
+
+    // Array method - 6
     newStr3.splice(1,1, 'stardust');
-    document.write("adding stardust:" + newStr3 + "<br>");
+    document.write("<strong>Adding stardust: </strong>" + newStr3 + "<br>");
+    // Array method - 7
     let newStr6 = newStr3.sort();
-    document.write("Alphabetical order: " + newStr6 + "<br>");
+    document.write("<strong>Alphabetical order: </strong>" + newStr6 + "<br>");
 }
