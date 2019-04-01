3706a5429dd3dd58510e09dabe609ba90b42b6b0
OldWorldForest <oldworldforest@gmail.com>
2019-03-31 22:32:15 -0700

    some changes

--- HW5/HW5.js
+++ HW5/HW5.js
@@ -1,19 +1,10 @@
-//start();
 update();
 
 let bool = false;
-//let firstList = document.getElementsByTagName('ul')[0];
-
-//function start() {
-//    document.getElementById('button').addEventListener('click', function(event) {
-//        action();
-//    });
-//}
 
 function action() {
     let userInput = prompt("Add a new quality to the list:");
     bool = true;
-    console.log(userInput);
 
     let newItem = document.createElement('li');
     let newQuality = document.createTextNode(userInput);
@@ -28,61 +19,78 @@ function update(bool) {
     let count = document.getElementById("qualities").childElementCount;
     document.getElementById("listStats").innerHTML = "Total Qualities: " + count;
 
-/*
-
-    let confirmAction = document.createElement("P");
-    confirmAction.innerHTML = "New item has been added to the list";
-    console.log("New item has been added");
-    document.getElementById("real").appendChild(confirmAction);
-*/
-
-
-
-
-
     if(bool == true) {
         let confirmAction = document.createElement('p');
 
         confirmAction.innerHTML = "New item has been added to the list";
-        console.log("New item has been added");
-
         document.getElementById('real').appendChild(confirmAction);
     }
 
-/*
 
-    if(bool == true) {
-        document.getElementById("stuff").innerHTML = "New item has been added to the list";
-        console.log("New item has been added");
-    } else if(bool == false) {
-        document.getElementById("stuff").innerHTML = "Item has been removed";
-        console.log("Item has been removed");
-    }
-
-     */
 
 }
 
 
 function highlight() {
     let list = document.getElementsByTagName('li');
+    let r1 = Math.floor((Math.random() * list.length));
+    let r2 = Math.floor((Math.random() * list.length));
+    let r3 = Math.floor((Math.random() * list.length));
+    let i1 = list[r1];
+    let i2 = list[r2];
+    let i3 = list[r3];
 
     if(list.length >= 3) {
-        let r1 = Math.floor((Math.random() * list.length));
-        let r2 = Math.floor((Math.random() * list.length));
-        let r3 = Math.floor((Math.random() * list.length));
-        let i1 = list[r1];
-        let i2 = list[r2];
-        let i3 = list[r3];
-        i1.className = 'must';
-        i2.className = 'must';
-        i3.className = 'must';
+
+        i1.className = 'must', i2.className = 'must', i3.className = 'must';
+
+        //let confirmAction = document.createElement('p');
+
+        //confirmAction.innerHTML = "New item has been added to the list";
+        //document.getElementById('real').appendChild(confirmAction);
+
 
     }
+
+    //displayHighlight(i1, i2, i3);
 }
 
 
+/* function displayHighlight(i1, i2, i3) {
 
+
+    console.log(i1);
+    console.log(i2);
+    console.log(i3);
+
+    let go2ul = document.getElementById("test");
+
+    go2ul.appendChild(i1);
+    let thisnode = document.createTextNode(i1);
+    //go2ul.appendChild(thisnode);
+
+
+    console.log(go2ul);
+    console.log(thisnode);
+    */
+
+
+
+/*
+    todo info i've passed the bullet points to this function now im getting confused on how to display them
+
+
+
+    let topItems = document.create('il');
+    let newQuality = document.createTextNode(i1);
+    topItems.appendChild(newQuality);
+
+    let firstList = document.createElement('ul')[1];
+    firstList.appendChild(topItems);
+
+ */
+
+}
 
 
 // TODO Create a second button that will call a function that will highlight three random qualities using Math.random() from the list.
@@ -90,4 +98,4 @@ function highlight() {
 // TODO --------
 // TODO Additionally add a paragraph that says "The top qualities are highlighted below" somewhere on the page.
 // TODO Create a third button that will call a function that will randomly remove 1 of the qualities from the list.
-// TODO Add a paragraph that confirms an item was removed.
+// TODO Add a paragraph that confirms an item was removed
